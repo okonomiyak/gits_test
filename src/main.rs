1018af1a14a152edacdb5522c43808a1dad81e37
okonomiyak <228486401+okonomiyak@users.noreply.github.com>
2026-09-25 11:36:44 +0900

    edit main make fizzbuzz

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    for i in 0..100{
+        match (i%3,i%5) {
+            (0,0) => println!("fizzbuzz"),
+            (0,_) => println!("buzz"),
+            (_,0) => println!("fizz"),
+            (_,_) => println!("{i}")
+        }
+    }
 }
